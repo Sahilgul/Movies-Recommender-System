60311104ec0f1ad0ef95ba965ddb22306c5af571
Sahil <82306344+Sahilgul@users.noreply.github.com>
2022-07-01 17:20:02 +0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -9,15 +9,6 @@ import requests
 
 mov_name = pd.read_csv('Movies Data/movies_title.csv')
 final_df = pickle.load(open('Movies Data/movies.pkl', 'rb'))
-# final_df.head()
-# similarity = pickle.load(open('Movies Data/cosine_sim.pkl', 'rb'))
-
-
-def fetch_poster(movie_id):
-    response = requests.get(
-        'https://api.themoviedb.org/3/movie/{}?api_key=34b55fa7f2d2346aa1045ed4cc22bd12&language=en-US'.format(movie_id))
-    data = response.json()
-    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 
 def create_similarity():
@@ -44,22 +35,11 @@ def recommend():
     distances = create_similarity()[index]
     movies_list = sorted(enumerate(distances),
                          reverse=True, key=lambda x: x[1])[0:11]
-
-    recomm_movies = []
-    recomm_poster = []
+    ids = []
     for i in movies_list:
         movie_id = final_df.loc[i[0]].id
-       # fetch Poster
-        # api = 34b55fa7f2d2346aa1045ed4cc22bd12
-        # api2 = https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
-        recomm_movies.append(final_df.loc[i[0]].title)
-        recomm_poster.append(fetch_poster(movie_id))
-
-    # return jsonify [{recomm_movies}:{recomm_poster}]
-    return '{} {}'.format(recomm_movies, recomm_poster)
-    # return jsonify(recomm_movies)
-    # return '{}'.format(recomm_movies)
-    # return jsonify({'Poster': recomm_movies})
+        ids.append(movie_id)
+    return '{}'.format(ids)
 
 
 if __name__ == '__main__':
